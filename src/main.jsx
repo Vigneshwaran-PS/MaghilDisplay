3a0404ed9f0247aa21baef5cddd2c1f8b9e2e7dc
Vignesh-PS <vigneshwaran.ps@magilhub.com>
2025-08-02 22:28:45 +0530

    fix : store import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import store, { persister } from 'src/redux/Store.jsx'
+import store, { persister } from './redux/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
